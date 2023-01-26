22dd8cedef4bc428485aa78c24e6323088d06013
sundaram123krishnan <krishnsundaram@gmail.com>
2023-01-26 19:49:12 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          950: 'hsla(237, 90.9%, 8.6%, 1);',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
